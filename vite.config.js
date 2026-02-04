4de250aa3d67f3b71ddc803371d55a82ec8caabb
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-02-04 14:15:32 +0100

    image optimization build error fix

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import imageOptimizer from "vite-plugin-image-optimizer";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    imageOptimizer({
+    ViteImageOptimizer({
       png: { quality: 80 },
       jpeg: { quality: 80 },
       jpg: { quality: 80 },
